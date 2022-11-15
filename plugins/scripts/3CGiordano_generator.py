cce90ffbc2b284d35ab977c4997b1c644829fcf1
santiago-giordano <100297383+santiago-giordano@users.noreply.github.com>
2022-11-15 20:55:48 -0300

    UP Normalized

--- plugins/scripts/3CGiordano_generator.py
+++ plugins/scripts/3CGiordano_generator.py
@@ -2,13 +2,13 @@ from jinja2 import Environment, FileSystemLoader
 import yaml
 import os
 
-file_dir = os.path.dirname(os.path.abspath(__file__))
+file_dir = os.path.dirname(os.path.abspath(f"{__file__}/../"))
 env = Environment(loader=FileSystemLoader(file_dir))
-template = env.get_template("G3Giordano_template_dag_etl.jinja2")
+template = env.get_template("templates/3CGiordano_template.jinja2")
 
 for filename in os.listdir(file_dir):
     if filename.endswith(".yaml"):
         with open(f"{file_dir}/{filename}", "r") as configfile:
             config = yaml.safe_load(configfile)
-            with open(f"dags/{config['dag_id']}.py", "w") as f:
+            with open(f"dags/3C{config['dag_id']}_dag_etl.py", "w") as f:
                 f.write(template.render(config))
