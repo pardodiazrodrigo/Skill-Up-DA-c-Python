9a1c8e15ea1aec035ff1362f0cd1f4bdc84d028b
nvrancovich <nvrancovich@gmail.com>
2022-11-15 13:13:39 -0300

    Add transform functions to dags

--- dags/3BUNComahue_dag_etl.py
+++ dags/3BUNComahue_dag_etl.py
@@ -40,7 +40,84 @@ with DAG(
 
     @task
     def comahue_transform():
-        pass
+        with open(
+            "/home/nvrancovich/airflow/dags/Skill-Up-DA-c-Python/datasets/3BUNComahue_select.csv", "r", encoding="utf-8"
+        ) as file:
+            df = pd.read_csv(file, index_col=[0])
+
+        cp = pd.read_csv('/home/nvrancovich/airflow/dags/Skill-Up-DA-c-Python/assets/codigos_postales.csv')
+
+        df["date_of_birth"] = pd.to_datetime(df["date_of_birth"], format="%Y-%m-%d")
+
+        def age(x):
+            today = date.today()
+            age = today.year - x.year - ((today.month, today.day) < (x.month, x.day))
+            return age
+
+        df["age"] = df["date_of_birth"].apply(age)
+        df = df.loc[df["age"].between(18, 90)]
+        
+        df['location'] = df['location'].astype(str)
+        
+        df.location = df.location.str.replace('_', ' ')
+
+        cp.rename(columns={"codigo_postal": "postal_code","localidad": "location",}, 
+                inplace=True)
+
+        df.drop(columns="postal_code",
+                inplace=True)
+
+        df = df.merge(cp, on="location", how="left")
+
+        words = [
+            "mr. ",
+            "mrs. ",
+            "miss ",
+            "dr. ",
+            " md",
+            " dds",
+            " phd",
+            " dvm",
+            " jr.",
+            " ii",
+            " iv",
+        ]
+
+        for word in words:
+            df.full_name = df.full_name.replace(word, '')
+            
+        df[["first_name", "last_name"]] = df.full_name.str.split(" ", n=1, expand=True)
+
+        df.drop(columns="full_name",
+                inplace=True)
+
+        df.gender = df.gender.replace('F', 'female')
+        df.gender = df.gender.replace('M', 'male')
+
+
+        df.university = df.university.str.replace('_', ' ')
+        df.career = df.career.str.replace('_', ' ')
+
+        df.university = df.university.str.lower()
+        df.career = df.career.str.lower()
+        df.first_name = df.first_name.str.lower()
+        df.last_name = df.last_name.str.lower()
+        df.location = df.location.str.lower()
+        df.email = df.email.str.lower()
+
+        df = df.reindex(columns=[
+                        "university",
+                        "career",
+                        "inscription_date",
+                        "first_name",
+                        "last_name",
+                        "gender",
+                        "age",
+                        "postal_code",
+                        "location", 
+                        "email"])
+
+        df.to_csv("/home/nvrancovich/airflow/dags/Skill-Up-DA-c-Python/3BUNSalvador_process.txt", sep="\t", index=None)
 
     @task
     def comahue_load():
--- dags/3BUNSalvador_dag_etl.py
+++ dags/3BUNSalvador_dag_etl.py
@@ -41,7 +41,76 @@ with DAG(
 
     @task
     def salvador_transform():
-        pass
+        with open(
+            "/home/nvrancovich/airflow/dags/Skill-Up-DA-c-Python/datasets/3BUNSalvador_select.csv", "r", encoding="utf-8"
+        ) as file:
+            df = pd.read_csv(file, index_col=[0])
+
+        cp = pd.read_csv('/home/nvrancovich/airflow/dags/Skill-Up-DA-c-Python/assets/codigos_postales.csv')
+
+        df["date_of_birth"] = pd.to_datetime(df["date_of_birth"], format="%Y-%m-%d")
+
+        def age(x):
+            today = date.today()
+            age = today.year - x.year - ((today.month, today.day) < (x.month, x.day))
+            return age
+
+        df["age"] = df["date_of_birth"].apply(age)
+        df = df.loc[df["age"].between(18, 90)]
+
+        cp.rename(columns={"codigo_postal": "postal_code","localidad": "location",}, 
+                inplace=True)
+
+        df.drop(columns="location",
+                inplace=True)
+
+        df = df.merge(cp, on="postal_code", how="left")
+
+        words = [
+            "mr. ",
+            "mrs. ",
+            "miss ",
+            "dr. ",
+            " md",
+            " dds",
+            " phd",
+            " dvm",
+            " jr.",
+            " ii",
+            " iv",
+        ]
+
+        for word in words:
+            df.full_name = df.full_name.replace(word, '')
+
+        df[["first_name", "last_name"]] = df.full_name.str.split("_", n=1, expand=True)
+
+        df.drop(columns="full_name",
+                inplace=True)
+
+        df.gender = df.gender.replace('F', 'female')
+        df.gender = df.gender.replace('M', 'male')
+
+        df.university = df.university.str.lower()
+        df.career = df.career.str.lower()
+        df.first_name = df.first_name.str.lower()
+        df.last_name = df.last_name.str.lower()
+        df.location = df.location.str.lower()
+        df.email = df.email.str.lower()
+
+        df = df.reindex(columns=[
+                        "university",
+                        "career",
+                        "inscription_date",
+                        "first_name",
+                        "last_name",
+                        "gender",
+                        "age",
+                        "postal_code",
+                        "location", 
+                        "email"])
+
+        df.to_csv("/home/nvrancovich/airflow/dags/Skill-Up-DA-c-Python/3BUNComahue_process.txt", sep="\t", index=None)
 
     @task
     def salvador_load():
