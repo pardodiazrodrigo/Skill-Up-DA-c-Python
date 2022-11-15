85a446c2d52bd4ce6c6deb7df5c94cbdcfd8f100
nvrancovich <nvrancovich@gmail.com>
2022-11-15 11:08:49 -0300

    Add dags

---
+++ dags/3BUNSalvador_dag_etl.py
@@ -0,0 +1,37 @@
+import os
+import boto3
+import pandas as pd
+from datetime import date, datetime, timedelta
+from airflow.models import DAG
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.decorators import dag, task
+
+default_args = {
+    "owner": "P3",
+    "retry_delay": timedelta(minutes=60),
+}
+
+with DAG(
+    'dag_universidad_del_Salvador',
+    description='DAG para la Universidad Del Salvador',
+    schedule_interval=timedelta(hours=1),
+    start_date=datetime(2022, 11, 3),
+    max_active_runs=5,
+    default_args=default_args,
+    catchup=False
+    
+) as dag:
+
+    @task()
+    def salvador_extract():
+        pass
+
+    @task
+    def salvador_transform():
+        pass
+
+    @task
+    def salvador_load():
+        pass
+
+    salvador_extract() >> salvador_transform() >> salvador_load()
