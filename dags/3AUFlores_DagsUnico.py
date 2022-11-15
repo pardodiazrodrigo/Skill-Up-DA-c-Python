93c479c4c7d33d3b2eef7945ff6d2366343e5f9e
Marrelli-Leandro <81393361+Marrelli-Leandro@users.noreply.github.com>
2022-11-15 11:18:37 -0300

    Transformation

--- dags/3AUFlores_DagsUnico.py
+++ dags/3AUFlores_DagsUnico.py
@@ -54,7 +54,86 @@ with DAG(
         @task()
 
         def Transformation():
-            pass
+            try:
+            
+                dfSel = pd.read_csv('files/3CUFlores_select.csv', sep=',')
+
+                dfSel = dfSel.astype({'last_name': 'string'})
+                dfSel = dfSel.astype({'first_name': 'string'})
+                dfSel = dfSel.astype({'university': 'string'})
+                dfSel = dfSel.astype({'career': 'string'})
+                dfSel = dfSel.astype({'location': 'string'})
+                dfSel = dfSel.astype({'email': 'string'}) 
+                # dfSel = dfSel.astype({'postal_code': 'string'})
+
+                
+                #################################### DIVISION DE CAMPO LAST_NAME #################################
+                dfSel["last_name"] = dfSel["last_name"].apply(lambda x: x.replace("MR. ",""))
+                dfSel["last_name"] = dfSel["last_name"].apply(lambda x: x.replace("DR. ",""))
+                dfSel["last_name"] = dfSel["last_name"].apply(lambda x: x.replace("MRS. ",""))
+                dfSel["last_name"] = dfSel["last_name"].apply(lambda x: x.replace(" MD",""))
+                dfSel["last_name"] = dfSel["last_name"].apply(lambda x: x.replace(" DDS",""))
+                dfSel["last_name"] = dfSel["last_name"].apply(lambda x: x.replace(" PHD",""))
+                dfSel["last_name"] = dfSel["last_name"].apply(lambda x: x.replace(" DVM",""))
+                dfSel["last_name"] = dfSel["last_name"].apply(lambda x: x.replace(" JR.",""))
+                dfSel["last_name"] = dfSel["last_name"].apply(lambda x: x.replace(" II",""))
+                dfSel["last_name"] = dfSel["last_name"].apply(lambda x: x.replace(" IV",""))
+                
+                dfSel['first_name'] = dfSel['last_name'].map(lambda x:x.split(' ') [0])
+                dfSel['last_name'] = dfSel['last_name'].map(lambda x:x.split(' ') [1])
+                ###################################################################################################
+
+                ########################## COMPLETAMINETO CAMPO POSTAL_CODE O LOCATION ############################         
+                dfCod = pd.read_csv('assets/codigos_postales.csv',sep=',')
+                dfCod = dfCod.drop_duplicates(['localidad'], keep='last')
+                df = pd.merge(dfSel,dfCod,left_on='postal_code',right_on='codigo_postal')
+                del df['codigo_postal']
+                del df['location']
+                del df['Unnamed: 0']
+                df = df.rename(columns={'localidad':'location'})
+                ###################################################################################################
+                #df.to_csv("/usr/local/airflow/include/3CUFlores_select_final.csv")
+                ############################## CAMBIO FECHA DE NACIMIENTO POR EDAD ################################
+                df["age"] = pd.to_datetime(df["age"], format="%Y/%m/%d")
+
+                def datediff(x):
+                    today = date.today()
+                    age = today.year - x.year - ((today.month, today.day) < (x.month, x.day))
+                    return age
+
+                df["age"] = df["age"].apply(datediff)
+                ###################################################################################################
+
+                ################################# FILTRACION POR EDAD #############################
+                df = df.loc[df["age"].between(18, 90)]
+                ###################################################################################
+
+                ########################### ELINACION DE PRE Y SUF ###########################
+                        
+                df["university"] = df["university"].str.lower()
+                df["career"] = df["career"].str.lower()
+                df["location"] = df["location"].str.lower()
+                df["first_name"] = df["first_name"].str.lower()
+                df["last_name"] = df["last_name"].str.lower()
+                df["email"] = df["email"].str.lower()
+
+                df = df.replace({'gender': {'F':'female','M':'male'}})
+                
+                df.career = df.career.str.strip()
+
+                df = df.reindex(columns= ['university','career','inscription_date','first_name','last_name','gender','age','postal_code','location','email'])
+
+                df = df.astype({'inscription_date': 'string'})
+                df = df.astype({'first_name': 'string'})
+                df = df.astype({'last_name': 'string'})
+                df = df.astype({'gender': 'string'})
+                df = df.astype({'postal_code': 'string'})
+                df = df.astype({'location': 'string'}) 
+                
+                df.to_csv("datasets/3AUFlores_process.txt", sep="\t", index=None)
+                logging.info('Tarea de transformacion EXITOSA')
+            except:
+                logging.info('ERROR al transformar')
         @task()
 
         def Load():
--- dags/3AUVillaMaria_DagsUnico.py
+++ dags/3AUVillaMaria_DagsUnico.py
@@ -56,7 +56,87 @@ with DAG(
         @task()
 
         def Transformation():
-            pass
+            try:
+                dfSel = pd.read_csv('files/3CUVillaMaria_select.csv', sep=',')
+
+                dfSel = dfSel.astype({'last_name': 'string'})
+                dfSel = dfSel.astype({'first_name': 'string'})
+                dfSel = dfSel.astype({'university': 'string'})
+                dfSel = dfSel.astype({'career': 'string'})
+                dfSel = dfSel.astype({'location': 'string'})
+                dfSel = dfSel.astype({'email': 'string'})     
+
+            #################################### DIVISION DE CAMPO LAST_NAME ##################################
+                dfSel["last_name"] = dfSel["last_name"].apply(lambda x: x.replace("MR._",""))
+                dfSel["last_name"] = dfSel["last_name"].apply(lambda x: x.replace("DR._",""))
+                dfSel["last_name"] = dfSel["last_name"].apply(lambda x: x.replace("MRS._",""))
+            
+                dfSel['first_name'] = dfSel['last_name'].map(lambda x:x.split('_') [0])
+                dfSel['last_name'] = dfSel['last_name'].map(lambda x:x.split('_') [1])
+            ###################################################################################################
+
+
+            ########################## COMPLETAMINETO CAMPO POSTAL_CODE O LOCATION ############################         
+            
+                dfCod = pd.read_csv('assets/codigos_postales.csv',sep=',')
+                dfCod = dfCod.drop_duplicates(['localidad'], keep='last')
+                df = pd.merge(dfSel,dfCod,left_on='location',right_on='localidad')
+                del df['localidad']
+                del df['postal_code']
+                del df['Unnamed: 0']
+                df = df.rename(columns={'codigo_postal':'postal_code'})
+        
+            ###################################################################################################
+
+            ############################## CAMBIO FECHA DE NACIMIENTO POR EDAD ################################
+                df["age"] = pd.to_datetime(df["age"], format="%d-%b-%y")
+
+                def datediff(x):
+                    today = date.today()
+                    age = today.year - x.year - ((today.month, today.day) < (x.month, x.day))
+                    return age
+
+                df["age"] = df["age"].apply(datediff)
+            ###################################################################################################
+
+            # df["inscription_date"] = pd.to_datetime(df["inscription_date"], format="%d-%b-%y")
+
+            ################################# FILTRACION POR EDAD #############################
+                df = df.loc[df["age"].between(18, 90)]
+            ###################################################################################
+
+            #################################### NORMALIZACION #################################
+            
+                df["university"] = df["university"].apply(lambda x: x.replace("_"," "))
+                df["career"] = df["career"].apply(lambda x: x.replace("_"," "))
+                df["location"] = df["location"].apply(lambda x: x.replace("_"," "))
+
+                df["university"] = df["university"].str.lower()
+                df["career"] = df["career"].str.lower()
+                df["location"] = df["location"].str.lower()
+                df["first_name"] = df["first_name"].str.lower()
+                df["last_name"] = df["last_name"].str.lower()
+                df["email"] = df["email"].str.lower()
+
+                df = df.replace({'gender': {'F':'female','M':'male'}})
+
+                df = df.reindex(columns= ['university','career','inscription_date','first_name','last_name','gender','age','postal_code','location','email'])
+
+                df = df.astype({'inscription_date': 'string'})
+                df = df.astype({'first_name': 'string'})
+                df = df.astype({'last_name': 'string'})
+                df = df.astype({'gender': 'string'})
+                df = df.astype({'postal_code': 'string'})
+                df = df.astype({'location': 'string'}) 
+
+            # del df['Unnamed: 0']
+            ###################################################################################################
+            
+                df.to_csv("datasets/3AUVillaMaria_process.txt", sep="\t", index=None)
+
+                logging.info('Tarea de transformacion EXITOSA')
+            except:
+                logging.info('ERROR al transformar')
         
         @task()
 
